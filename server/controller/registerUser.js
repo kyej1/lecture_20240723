d5a0e5502f9b22ca21e262e9546f00053b2b80bc
kyej1 <146058106+kyej1@users.noreply.github.com>
2024-07-25 03:56:52 +0000

    7월 25일 수업 내용임

--- server/controller/registerUser.js
+++ server/controller/registerUser.js
@@ -1,6 +1,26 @@
-function registerUser(req,res){
+const UserModel = require("../models/UserModel.js")
+async function registerUser(req,res){
+    const {name, email, password, profile_pic} = req.body
+    console.log(`name: ${name}`)
+
+    const checkEmail = await UserModel.findOne({email})
+    if (checkEmail){
+        return res.status(400).json({
+            message : "헉, 있는 사용자입니다. 다른 이메일을 사용해주세요."
+        })
+    }
+    const payload = {
+        name,
+        email,
+        password,
+        profile_pic
+    }
+
+    const user = new UserModel(payload)
+    const userSave = await user.save()
+
     return res.status(201).json({
-        message: "사용자를 만들어볼까요?"
+        message: "wow! 회원등록이 성공되었습니다. 축하드려요!"
     })
         
 }
